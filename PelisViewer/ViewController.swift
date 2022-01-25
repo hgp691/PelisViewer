a4faecb7ec18d1749e46349de37ef5ae0f2ba312
Horacio Guzmán Parra <hgp691@gmail.com>
2022-01-24 20:17:16 -0500

    Project created

---
+++ PelisViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PelisViewer
+//
+//  Created by Horacio Guzman on 24/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
